e59adff792746bcf5a8ef11305f0d981ac4e215d
Oleg <41553174+ChelovekOleg@users.noreply.github.com>
2021-06-17 23:58:24 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "test";
+$url = "https://xkcd.com/info.0.json";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+
+$resp = curl_exec($curl);
+curl_close($curl);
+var_dump($resp);
