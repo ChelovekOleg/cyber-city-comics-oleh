669902f588277c01367265b0d661b5ba72d2ef3a
Oleg <41553174+ChelovekOleg@users.noreply.github.com>
2021-06-18 00:01:40 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,13 @@
 <?php
 
-$url = "https://xkcd.com/info.0.json";
+$num = $_GET["num"];
+
+if($num > 0){
+    $url = "https://xkcd.com/".$num ."/info.0.json";
+}else{
+    $url = "https://xkcd.com/info.0.json";
+}
+
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -13,4 +20,5 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $resp = curl_exec($curl);
 curl_close($curl);
-var_dump($resp);
+header('Content-Type: application/json');
+echo json_encode($resp);
